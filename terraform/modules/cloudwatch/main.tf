953882a17b85f4dc470a088b34d513dcba76ccdd
HadeerAlaa542 <hadeeralaa542@gmail.com>
2025-03-24 05:29:14 +0200

    Delete terraform directory

--- terraform/modules/cloudwatch/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "app_logs" {
-  name              = "/aws/ec2/app"
-  retention_in_days = 7
-}
-
-output "cloudwatch_log_group" {
-  value = aws_cloudwatch_log_group.app_logs.name
-}
-
